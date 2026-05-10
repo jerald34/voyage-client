9da9519fcf1a1e3c196377699b9bbd14ebd6c5ee
Jerald34 <deverajerald0@gmail.com>
2026-05-10 20:21:10 +0800

    Revert "feat: implement core client-side redesign with glassmorphism and bento-box landing"
    This reverts commit 0edce2c701e2820d479e623589a24149c4e89120.

--- app/components/share/GlassPanel.jsx
+++
@@ -1,7 +0,0 @@
-export default function GlassPanel({ children }) {
-  return (
-    <div className="absolute top-8 left-8 bottom-8 w-96 glass-panel overflow-y-auto z-10 flex flex-col p-6">
-      {children}
-    </div>
-  );
-}
